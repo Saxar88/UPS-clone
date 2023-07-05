84399065f493acfb167e7d94b32f4b5db7af5cb3
Saxar88 <v.sasaran@protonmail.com>
2023-07-05 18:40:26 +0300

    build useCustomerOrders and useOrders hooks

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,23 @@
 type Customer = { email: string; name: string };
 
 type CustomerList = { name: ID; value: Customer };
+
+type TrackingItems = { customer_id: ID; customer: Customer; items: Item[] };
+
+type Item = { item_id: ID; name: string; price: number; quantity: number };
+
+type Order = {
+	carrier: string;
+	createdAt: string;
+	shippingCost: number;
+	trackingId: string;
+	trackingItems: TrackingItems;
+	Lat: number;
+	Lng: number;
+	Address: string;
+	City: string;
+};
+
+type OrderResponse = { value: Order };
+
+type CustomerResponse = { name: ID; value: Customer };
